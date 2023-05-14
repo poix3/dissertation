dfe649fb6ce77b4ded3579ef12a908407b45f417
poix3 <scu_wong@outlook.com>
2023-05-14 22:21:41 +0200

    fix util.py

--- util.py
+++ util.py
@@ -1,6 +1,7 @@
 import os
 os.environ["DGLBACKEND"] = "pytorch"
 
+import re
 import dgl
 import json
 import torch
@@ -15,7 +16,8 @@ from transformers import AutoModel
 from data.flota import FlotaTokenizer
 
 MIN_LEN = 5
-MAX_LEN = 100
+MAX_LEN = 100 # max length of a sentence
+MAX_SENT = 100 # max number of sentences in a case
 NUM_LABELS = 10 # from ecthr dataset
 VOCAB_SIZE = 5000
 
@@ -31,18 +33,24 @@ def get_sent_length(sent: str):
     return len(sent.split())
 
 def preprocess_case(legal_case, return_length=False):
-    """split sentences and truncate"""
+    """split sentences
+       truncate both sentences and number of sentences"""
     sent_len = []
+    sent_cnt = 0
     sentences = []
     for sent in nlp(legal_case).sents:
+        if sent_cnt > MAX_SENT and not return_length:
+            break # truncate the case
         sent = sent.text
         length = get_sent_length(sent)
         if length < MIN_LEN:
             continue 
         if length > MAX_LEN:
+            # truncate the sentence
             sent = " ".join(sent.split()[:MAX_LEN])
         sent_len.append(length)
         sentences.append(sent)
+        sent_cnt += 1
 
     if return_length:
         return sentences, sent_len
@@ -127,18 +135,19 @@ def record_and_save(dataset, vocab_size = VOCAB_SIZE):
     torch.save(cons_label, "cons_label.pt")
 
 @torch.no_grad()
-def sent2graph(dataset, split):
+def sent2graph(dataset):
     vocab_table = torch.load("data/vocab_table.pt")
     with open("data/edge_table.json") as f:
         edge_table = json.load(f)
 
     for split in ["train","validation", "test"]:
         split_dataset = dataset[split]
-        graph_x = []
+        graph_x = [] # batched graphs of cases
         graph_y = label2onehot(split_dataset["labels"])
         for legal_case in split_dataset["text"]:
             sentences = preprocess_case(legal_case)
             amr_graphs = stog.parse_sents(sentences)
+            sent_graph = [] # graphs for sentences in a case
             for amr_graph in amr_graphs:
                 if not isinstance(amr_graph, str):
                     continue
@@ -158,35 +167,43 @@ def sent2graph(dataset, split):
                     edge_ids.append(edge_table.get(edge.role, 0))
 
                 dgl_graph = dgl.graph((src, dst), num_nodes=len(node_ref))
-                # to undirected
-                dgl_graph = dgl.to_bidirected(dgl_graph)
-                edge_ids = edge_ids * 2
                 # save top node index
                 top_index = node_ref.get(penman_graph.top)
                 top = torch.zeros(dgl_graph.num_nodes())
                 top[top_index] = 1
                 # assign features to dgl graph
-                dgl_graph.ndata["top"] = top
+                dgl_graph.ndata["top_mask"] = top
                 dgl_graph.ndata["node_ids"] = torch.LongTensor(node_ids)
                 dgl_graph.edata["edge_ids"] = torch.LongTensor(edge_ids)
-                graph_x.append(dgl_graph)   
+                # to undirected
+                dgl_graph = dgl.add_reverse_edges(dgl_graph, copy_edata=True)
+                sent_graph.append(dgl_graph)   
+
+            graph_x.append(dgl.batch(sent_graph))
         # save to disk
         graph_dataset = {"x" : graph_x, "y" : graph_y}
         torch.save(graph_dataset, f"{split}_graph_dataset.pt")
 
+def process_word(word):
+    if "-" in word:
+        word = re.sub(r"-\d+$", "", word) # remove digits
+        word = " ".join(word.split("-"))
+
+    return word
+    
 @torch.no_grad()   
 def get_initial_embedding_weight():
     """get initial node embedding from bert using FLOTA"""
     bert = AutoModel.from_pretrained("bert-base-uncased", 
                                       output_hidden_states=True)
     tokenizer = FlotaTokenizer("bert-base-uncased", k = 3)
-    vocab_table = torch.load("vocab_table.pt")
+    vocab_table = torch.load("data/vocab_table.pt")
 
     initial_node_embedding = []
     initial_node_embedding.append(torch.randn(768)) # "other" -> 0
-    for vocab in vocab_table.keys():
-        words = " ".join(vocab.split("-")[:-1])
-        input = tokenizer(words, return_tensors="pt")
+    for word in vocab_table.keys():
+        word = process_word(word)
+        input = tokenizer(word, return_tensors="pt")
         output = bert(**input)
         embedding_output = output[2][0]
         # average
@@ -194,11 +211,5 @@ def get_initial_embedding_weight():
         initial_node_embedding.append(vocab_embedding)
     
     initial_node_embedding = torch.stack(initial_node_embedding, dim=0)
-    torch.save(initial_node_embedding, "data/initial_node_embedding.pt")
-    # torch.save(bert.embeddings.position_embeddings.weight, "data/initial_pos_embedding.pt" )
-    
-def main():
-    pass
-
-if __name__ == "__main__":
-    main()
+    torch.save(initial_node_embedding, "initial_node_embedding.pt")
+    torch.save(bert.embeddings.position_embeddings.weight, "initial_pos_embedding.pt")
