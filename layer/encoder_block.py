bf98ebd91aac2910fca15d4980d98d3f3fb6a55d
poix3 <scu_wong@outlook.com>
2023-05-18 18:27:41 +0200

    fix attention and pooling

--- layer/encoder_block.py
+++ layer/encoder_block.py
@@ -16,16 +16,6 @@ class Encoder(nn.Module):
         bert = BertModel.from_pretrained("bert-base-uncased")
         return bert.encoder.layer[id]
 
-    def get_attention_mask(self, top_count):
-        attention_mask = []
-        max_len = max(top_count)
-        for cnt in top_count:
-            row = [1] * cnt + [0] * (max_len - cnt)
-            attention_mask.append(row)
-        
-        attention_mask = torch.FloatTensor(attention_mask)
-        return attention_mask
-
     def get_extended_attention_mask(self, attention_mask, dtype=torch.float):
         """https://github.com/huggingface/transformers/blob/v4.29.1/src/transformers/modeling_utils.py#L873-L895"""
         extended_attention_mask = attention_mask[:, None, None, :]
@@ -33,31 +23,25 @@ class Encoder(nn.Module):
         extended_attention_mask = (1.0 - extended_attention_mask) * torch.finfo(dtype).min
         return extended_attention_mask
 
-    def forward(self, bbg, node_split, edge_split):
+    def forward(self, bbg, attention_mask, node_split, edge_split):
         """ bbg: b  - num traning sample / batch size
-                 bg - batched graph from a case
-            node_split: num_nodes of bgs
-            edge_split: num_edges of bgs """
+                 bg - batched graph from a case """
         bbg = self.graph_encoder(bbg)
         bgs = dgl.unbatch(bbg, node_split, edge_split) # list
         # prepare data for CSE
         batched_top_feat = []
-        top_count = [] 
         for bg in bgs:
             top_mask = bg.ndata["top_mask"]
             top_feat = bg.ndata["node_feat"][top_mask == 1] # dim: [num_top_nodes, feat_dim]
             batched_top_feat.append(top_feat)
-            top_count.append(top_feat.shape[0])
 
         batched_top_feat = pad_sequence(batched_top_feat, batch_first=True) # dim: [batch_size, max_num_top_nodes, feat_dim]
-        attention_mask = self.get_attention_mask(top_count) # dim: [batch_size, max_num_top_nodes]
         extended_attention_mask = self.get_extended_attention_mask(attention_mask)
         batched_top_feat = self.cross_segment_encoder(batched_top_feat, extended_attention_mask)[0]
         # update top nodes
-        for i, (att_mask, bg) in enumerate(zip(attention_mask, bgs)):
-            top_feat = batched_top_feat[i]
-            indices = torch.nonzero(att_mask).squeeze()
-            top_feat = top_feat[indices] # unpad
+        for top_feat, att_mask, bg in zip(batched_top_feat, attention_mask, bgs):
+            num_top = sum(att_mask == 1)
+            top_feat = top_feat[:num_top] # unpad
             # update tops
             top_mask = bg.ndata["top_mask"]
             bg.ndata["node_feat"][top_mask == 1] = top_feat
--- layer/model.py
+++ layer/model.py
@@ -10,21 +10,50 @@ class Net(nn.Module):
         super().__init__()
         self.embedding = GraphEmbedding(num_e_labels, e_dim)
         self.encoder = nn.ModuleList([Encoder(id) for id in range(num_layer)])
-        self.max_pooling_proj = nn.Sequential(nn.Linear(out_dim, out_dim//6),
-                                   nn.Tanh(),
-                                   nn.Linear(out_dim//6, 10))
+        self.proj = nn.Sequential(nn.Linear(out_dim, out_dim),
+                                  nn.Tanh(),
+                                  nn.Linear(out_dim, 10))
 
-    def forward(self, bg_list):
+    def get_attention_mask(self, bgs):
+        # record number of top nodes
+        top_count = []
+        for bg in bgs:
+            top_mask = bg.ndata["top_mask"]
+            num_top = sum(top_mask == 1)
+            top_count.append(num_top)
+        # create attention mask
+        attention_mask = []
+        max_len = max(top_count)
+        for cnt in top_count:
+            row = [1] * cnt + [0] * (max_len - cnt)
+            attention_mask.append(row)
+        
+        attention_mask = torch.FloatTensor(attention_mask)
+        return attention_mask
+    
+    def max_pooling(self, cse_out, attention_mask):
+        """cse_out, dim: [batch_size, max_num_top_nodes, feat_dim]
+           attention_mask, dim: [batch_size, max_num_top_nodes]"""
+        pooled = []
+        for out, att_mask in zip(cse_out, attention_mask):
+            num_top = sum(att_mask == 1)
+            out = out[:num_top]
+            pooled.append(torch.max(out, dim=0).values)
+
+        return torch.stack(pooled, dim=0)
+
+    def forward(self, bgs):
         """input: a list batched DGLGraphs"""
         # record information for unbatch operation
-        node_split = torch.tensor([bg.num_nodes() for bg in bg_list])
-        edge_split = torch.tensor([bg.num_edges() for bg in bg_list])
+        node_split = torch.tensor([bg.num_nodes() for bg in bgs])
+        edge_split = torch.tensor([bg.num_edges() for bg in bgs])
 
-        bg_list = [self.embedding(bg) for bg in bg_list]
-        bbg = dgl.batch(bg_list)
+        attention_mask = self.get_attention_mask(bgs) # dim: [batch_size, max_num_top_nodes]
+        bgs = [self.embedding(bg) for bg in bgs]
+        bbg = dgl.batch(bgs)
         for layer in self.encoder:
-            bbg, cse_out = layer(bbg, node_split, edge_split)
+            bbg, cse_out = layer(bbg, attention_mask, node_split, edge_split)
         
-        pooled = torch.max(cse_out, dim=1).values
-        logits = self.max_pooling_proj(pooled)
+        pooled = self.max_pooling(cse_out, attention_mask) # dim: [batch_size, feat_dim]
+        logits = self.proj(pooled)
         return logits
